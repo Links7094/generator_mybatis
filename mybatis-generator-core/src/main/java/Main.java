9f6372ec0cda2a274c47793a466538ea43342a79
links <links@idreamtech.com.cn>
2018-08-09 16:33:25 +0800

    init

---
+++ mybatis-generator-core/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Main {
+
+    private static final String CONFIG_PATH = Objects.requireNonNull(
+            Main.class.getClassLoader().getResource("MyBatisGeneratorConfig.xml")).getPath();
+
+    public static void main(String[] args) throws Exception {
+        File configFile = new File(CONFIG_PATH);
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
